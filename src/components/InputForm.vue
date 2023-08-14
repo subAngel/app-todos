e4e8d83f4857357a0699364e88913aae7c7525bf
Angel Jesus Zorrilla Cuevas <whoangel.agl@gmail.com>
2023-08-14 06:31:59 -0600

    Creando componente dinamico para el input del formulario

---
+++ src/components/InputForm.vue
@@ -0,0 +1,28 @@
+<template>
+	<div class="form-control">
+		<label class="label">
+			<span
+				class="label-text text-gray-500 font-bold md:text-right mb-2 md:mb-0"
+				>{{ titleLabel }}</span
+			>
+		</label>
+		<input
+			:type="typeInput"
+			:placeholder="placeholder"
+			class="input input-bordered"
+			:value="modelValue"
+			@input="$emit('update:modelValue', $event.target.value)"
+		/>
+		<label class="label">
+			<span class="label-text-alt text-error font-semibold text-sm">
+				Bottom Left label
+			</span>
+		</label>
+	</div>
+</template>
+
+<script setup>
+import { defineProps, ref } from "vue";
+const props = defineProps(["titleLabel", "typeInput", "placeholder", "modelValue"]);
+const emits = defineEmits(["update:modelValue"]);
+</script>
--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,1 +1,31 @@
-<template>Login</template>
+<template>
+	<main class="w-full min-h-screen flex justify-center items-center">
+		<form class="shadow-md rounded px-8 pt-6 pb-8 mb-4 bg-base-200 w-1/5">
+			<h3 class="font-medium text-3xl text-center">Login</h3>
+			<InputForm
+				titleLabel="Email"
+				placeholder="Email"
+				typeInput="email"
+				v-model="email"
+			/>
+			<InputForm
+				titleLabel="Password"
+				placeholder="Password"
+				typeInput="password"
+				v-model="password"
+			/>
+
+			<div class="form-control mt-6">
+				<button class="btn btn-primary">Login</button>
+			</div>
+		</form>
+	</main>
+</template>
+
+<script setup>
+import { ref } from "vue";
+import InputForm from "../components/InputForm.vue";
+
+const email = ref("");
+const password = ref("");
+</script>
