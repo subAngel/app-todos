2afbba2c7dc4932d5bce5823b32c43a79caab6fb
Angel Zorrilla <whoangel.agl@gmail.com>
2023-08-13 21:12:12 -0600

    instalando daisyui pero sale un error

--- src/App.vue
+++ src/App.vue
@@ -2,12 +2,13 @@
 
 <template>
 	<div>
-		<a href="https://vitejs.dev" target="_blank">
-			<img src="/vite.svg" class="logo" alt="Vite logo" />
-		</a>
-		<a href="https://vuejs.org/" target="_blank">
-			<img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-		</a>
+		<button class="btn">Button</button>
+		<button class="btn btn-neutral">Neutral</button>
+		<button class="btn btn-primary">Primary</button>
+		<button class="btn btn-secondary">Secondary</button>
+		<button class="btn btn-accent">Accent</button>
+		<button class="btn btn-ghost">Ghost</button>
+		<button class="btn btn-link">Link</button>
 	</div>
 </template>
 
