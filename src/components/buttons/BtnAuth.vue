e579116336f0b52d362e7e7f14600a141ad4dae8
Angel Jesus Zorrilla Cuevas <whoangel.agl@gmail.com>
2023-08-16 21:04:46 -0600

    Creando un componente de boton para formularios reutilizable

---
+++ src/components/buttons/BtnAuth.vue
@@ -0,0 +1,25 @@
+<template>
+  <button class=" bg-primary rounded-lg py-3 h-12 text-primary-content font-bold text-lg flex justify-center items-center focus:bg-primary-focus disabled:opacity-60 " tabindex="-1" role="button" aria-disabled="true"
+			:class="{'isDisabled': isLoading}"
+				:disabled="isLoading || isDisabled"
+				>
+				<Loader v-if="isLoading"/>
+				<span v-else>{{text}}</span>
+			</button>
+</template>
+
+<script setup>
+import { defineProps } from 'vue'
+import Loader from '../loaders/Loader.vue'
+
+const props = defineProps(['text', 'isDisabled', 'isLoading'])
+// const emit = defineEmits(['submit'])
+
+</script>
+
+<style scoped>
+button:disabled{
+  opacity: 60%;
+}
+
+</style>
--- src/components/forms/LoginForm.vue
+++ src/components/forms/LoginForm.vue
@@ -1,6 +1,5 @@
 <template>
 	<form
-		@submit.prevent="onSubmit"
 		class="bg-white shadow-md rounded-lg px-8 pt-6 pb-8 mb-4 max-w-sm w-3/5"
 	>
 		<h2 class="text-4xl font-medium text-slate-800 text-center mb-5">Log In</h2>
@@ -22,8 +21,15 @@
 		/>
 		<!-- <pre>{{ password }}</pre> -->
 
+
 		<div class="form-control mt-6">
-			<button class="btn btn-primary" :disabled="isDisabled">Login</button>
+			<BtnAuth text="Login" :isDisabled="isDisabled" :isLoading="isLoading" @click.prevent="onSubmit"/>
+		</div>
+
+		<div class="mt-6 text-center text-sm font-semibold" >
+				<span class="text-center">Don't have an account? <router-link
+					class="link link-hover link-primary"
+					to="/register">Sign up</router-link></span>
 		</div>
 
 	</form>
@@ -33,19 +39,21 @@
 import { ref, reactive, onMounted, computed } from "vue";
 import { useForm } from "vee-validate";
 import CustomInput from "../inputs/InputValidated.vue";
+import Loader from "../loaders/Loader.vue";
 import * as yup from "yup";
 import axios from 'axios'
 import { useToast } from "vue-toastification";
-import { Notyf } from 'notyf'
 import 'notyf/notyf.min.css'; // for React, Vue and Svelte
 
-
 import auth from "../../auth";
+import BtnAuth from "../buttons/BtnAuth.vue";
 
 const email = ref("");
 const password = ref("");
 
 const toast = useToast()
+const isLoading = ref(false)
+
 const { errors, handleSubmit, defineInputBinds } = useForm({
 	validationSchema: yup.object({
 		email: yup.string().email().required(),
@@ -58,18 +66,28 @@ const { errors, handleSubmit, defineInputBinds } = useForm({
 });
 
 const onSubmit = handleSubmit((values) => {
-
-	axios.post('https://api-todos-enwu.onrender.com/api/auth/login', values).then(res => {
-		console.log(res.data)
-	}).catch(err => {
-		const errorMessage = err.response.data.message
-		// notyf.error(errorMessage)
-		toast.error(errorMessage)
-	})
-	// alert(JSON.stringify(values, null, 2));
+	login(values)
 });
+
+const login = async (user) => {
+	isLoading.value = true
+	axios.post('https://api-todos-enwu.onrender.com/api/auth/login',
+		user).then(res => {
+			console.log(res.data);
+		}).catch(err => {
+			toast.error(err.response.data.message)
+		}).finally(() => {
+			isLoading.value = false
+		})
+}
 
 const isDisabled = computed(() => {
-	return email.value === "" || password.value === "" || password.value.length < 6;
+	return email.value === "" || password.value === "" || password.value.length < 6 || isLoading.value==true;
 });
 </script>
+
+<style scoped>
+.isDisabled{
+	opacity: 60%;
+}
+</style>
