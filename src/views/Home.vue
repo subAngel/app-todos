6778ca28077833e83719ea7ecf3cb858cbade622
Angel Jesus Zorrilla Cuevas <whoangel.agl@gmail.com>
2023-08-17 22:06:37 -0600

    Estilizando el active router link

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,11 +1,15 @@
 <template>
     <Navbar :fullname="user.fullname" :username="user.username" />
 
-    <main class="mx-auto w-9/12 pt-28">
-        <router-link :to="`/${$route.params.username}/tasks`">
-            Taks
-        </router-link>
-        <router-link :to="`/${$route.params.username}/completed-tasks`">Completed Tasks</router-link>
+    <main class="mx-auto w-9/12 pt-5">
+        <div class="w-3/6 mx-auto flex items-center content-center justify-center mb-5">
+            <router-link class=" w-1/2 text-center text-xl" :to="`/${$route.params.username}/tasks`">
+                <span>Tasks</span>
+            </router-link>
+            <router-link class=" w-1/2 text-center text-xl" :to="`/${$route.params.username}/completed-tasks`">
+                <span>Completed Tasks</span>
+            </router-link>
+        </div>
         <router-view></router-view>
     </main>
     <Modal />
@@ -49,3 +53,71 @@ onMounted(() => {
 
 
 </script>
+
+<style scoped>
+.router-link-active {
+    position: relative;
+    height: 40px;
+    padding: 0 26px;
+    border: 2px solid #000;
+    background: #e8e8e8;
+    user-select: none;
+    white-space: nowrap;
+    transition: all .05s linear;
+    font-family: inherit;
+}
+
+.router-link-active:before,
+.router-link-active:after {
+    content: "";
+    position: absolute;
+    background: #e8e8e8;
+    transition: all .2s linear;
+}
+
+.router-link-active:before {
+    width: calc(100% + 6px);
+    height: calc(100% - 16px);
+    top: 8px;
+    left: -3px;
+}
+
+.router-link-active:after {
+    width: calc(100% - 16px);
+    height: calc(100% + 6px);
+    top: -3px;
+    left: 8px;
+}
+
+.router-link-active:hover {
+    cursor: crosshair;
+}
+
+.router-link-active:active {
+    transform: scale(0.95);
+}
+
+.router-link-active:hover:before {
+    height: calc(100% - 32px);
+    top: 16px;
+}
+
+.router-link-active:hover:after {
+    width: calc(100% - 32px);
+    left: 16px;
+}
+
+.router-link-active span {
+    font-size: 16px;
+    z-index: 3;
+    position: relative;
+    font-weight: 600;
+}
+
+span {
+    font-size: 16px;
+    z-index: 3;
+    position: relative;
+    font-weight: 600;
+}
+</style>
