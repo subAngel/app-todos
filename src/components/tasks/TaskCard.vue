8b35ba4d226dbe202d53e1c63a271df92b6c2324
Angel Jesus Zorrilla Cuevas <whoangel.agl@gmail.com>
2023-08-17 21:21:34 -0600

    Agregar un loader cuando cargan las tareas

--- src/components/tasks/TaskCard.vue
+++ src/components/tasks/TaskCard.vue
@@ -1,8 +1,24 @@
 <template>
-    <div class="bg-accent-content w-10/12">
-        <label>{{ title }}</label>
-        <label>{{ description }}</label>
+    <div class="bg-accent-content py-5 rounded-xl border px-2">
+        <h3 class="font-bold text-sm">{{ title }}</h3>
+        <p class="text-xs italic">{{ description }}</p>
+
     </div>
+
+
+    <!-- <div class="indicator ">
+        <div class="indicator-item indicator-bottom">
+            <button class="btn btn-primary">Apply</button>
+            <button class="btn btn-primary">Apply</button>
+            <button class="btn btn-primary">Apply</button>
+        </div>
+        <div class="card border">
+            <div class="card-body">
+                <h2 class="card-title font-bold">{{ title }}</h2>
+                <p>{{ description }}</p>
+            </div>
+        </div>
+    </div> -->
 </template>
 
 <script setup>
--- src/views/Tasks.vue
+++ src/views/Tasks.vue
@@ -1,7 +1,10 @@
 <template>
     <div class="mockup-window border bg-base-300">
-        <div class="bg-base-200 grid grid-cols-1 grid-rows-2 gap-5  mx-auto">
-            <div v-for="task in tasks" :key="task.id" class="">
+        <div class="bg-base-200 grid grid-cols-1 grid-rows-2 gap-5 py-5">
+            <div v-if="isLoading" class="place-self-center">
+                <Loader2 />
+            </div>
+            <div v-for="task in tasks" :key="task.id" class="mx-auto w-5/6 ">
                 <TaskCard :title="task.title" :description="task.description" />
             </div>
         </div>
@@ -15,19 +18,24 @@ import auth from '../utils/auth';
 import { useToast } from 'vue-toastification'
 
 import TaskCard from '../components/tasks/TaskCard.vue'
+import Loader2 from '../components/loaders/Loader2.vue'
 
 const toast = useToast()
 
 const tasks = ref([])
+const isLoading = ref(false)
 
 onMounted(() => {
     const token = $cookies.get('auth')
+    isLoading.value = true
     auth.fetchTasks(token).then(res => {
         // console.log(res.data);
         tasks.value = res.data
     }).catch(err => {
         // console.log(err);
         toast.error("Error loading the tasks")
+    }).finally(() => {
+        isLoading.value = false
     })
 })
 </script>
